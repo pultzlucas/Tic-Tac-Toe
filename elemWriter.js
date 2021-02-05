bfe929d682fc2d00898495718ec960dcd71d71db
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-02-05 08:24:43 -0300

    first commit

---
+++ elemWriter.js
@@ -0,0 +1,1 @@
+require('elements.dom')('index', './src/scripts/elements.js')
---
+++ src/scripts/elements.js
@@ -0,0 +1,8 @@
+// MAIN 
+const html = document.querySelector('html')
+const body = document.querySelector('body')
+// ID 
+// CLASS 
+const checkArea = document.querySelectorAll('.check_area')
+const container = document.querySelector('.container')
+const game = document.querySelector('.game')
